178c9d550598e03906f51406c57388ac5275d002
Edgar Pan <edgaryp@me.com>
2018-11-11 01:27:22 +1100

    Setup products grid

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/" exact>Home</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+    <!-- <v-container> -->
+      <router-view/>
+    <!-- </v-container> -->
+  </v-app>
 </template>
 
 <script>
@@ -14,6 +27,12 @@ import {mapState} from 'vuex'
 db.settings({ timestampsInSnapshots: true });
 
 export default {
+  name: 'App',
+  data() {
+    return {
+      //
+    };
+  },
   computed: {
     ...mapState([
       'products',
@@ -22,23 +41,6 @@ export default {
   }
 }
 </script>
+<style>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="home">
-    <ul>
-      <li v-for="(product, index) in products" :key="index">
-        <router-link :to="`/product/${getProductUrl(product.name)}`" exact>{{product.name}}</router-link>
-      </li>
-    </ul>
-  </div>
+  <v-content>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12 sm2>
+          <v-container>
+            <h2>filter goes here</h2>
+          </v-container>
+        </v-flex>
+        <v-flex xs12 sm10>
+          <v-container grid-list-md>
+              <v-layout row wrap>
+                <v-flex md3 sm6 xs12 d-flex v-for="(product, index) in products" :key="index" exact>
+                  <v-card flat :to="`/product/${getProductUrl(product.name)}`">
+                    <!-- <router-link :to="`/product/${getProductUrl(product.name)}`" exact> -->
+                    <v-card-text primary>
+                      <h3>{{product.name}}</h3>
+                      <p>{{product.shortDescription}}</p>
+                      <p>{{product.price}}</p>
+                    </v-card-text>
+                    <!-- </router-link> -->
+                  </v-card>
+                </v-flex>
+              </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+  </v-content>
 </template>
 
 <script>
