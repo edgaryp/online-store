c7ce5443373ae7fe75031f89e7ad81d33a3237bf
Edgar Pan <edgaryp@me.com>
2018-11-22 21:40:28 +1100

    Added static images for experimenting

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,14 +1,15 @@
 <template>
   <v-content>
     <v-container>
-      <v-layout>
+      <v-layout row wrap>
         <v-flex md6 sm12>
-          <div class="about">
-            <h1>Product: {{currentProductName}}</h1>
+            <v-carousel ref="carousel" :cycle="autoplay">
+              <v-carousel-item v-for="(image, index) in images" :key="index" :src="image"></v-carousel-item>
+            </v-carousel>
+            <!-- <h1>Product: {{currentProductName}}</h1>
             <p>{{currentProduct}}</p>
             <HelloWorld :currentProduct="currentProduct"/>
-            <button @click="test">Checkout</button>
-          </div>
+            <button @click="test">Checkout</button> -->
         </v-flex>
         <v-flex md6 sm12>
         </v-flex>
@@ -18,17 +19,31 @@
 </template>
 
 <script>
+/* eslint-disable */
 import {mapState, mapMutations, mapGetters} from 'vuex'
 import * as mutationTypes from '../store/mutation-types'
 import * as getterTypes from '../store/getter-types'
-import HelloWorld from '@/components/ProductPage/HelloWorld.vue'
+// import HelloWorld from '@/components/ProductPage/HelloWorld.vue'
 
 export default {
+  data() {
+    return {
+      // static for now
+      images: [
+        'https://picsum.photos/525/394?image=700',
+        'https://picsum.photos/525/394?image=1045',
+        'https://picsum.photos/525/394?image=878',
+        'https://picsum.photos/525/394?image=543',
+        'https://picsum.photos/525/394?image=1047'
+      ],
+      autoplay: false
+    }
+  },
   props: [
     'currentProductName' // from router
   ],
   components: {
-    HelloWorld
+    // HelloWorld
   },
   computed: {
     ...mapState([
@@ -42,7 +57,7 @@ export default {
     ...mapMutations({
       setCurrentProductName: mutationTypes.SET_CURRENT_PRODUCT_NAME
     }),
-    test() {
+    goBasket() {
       this.$router.push({
         name: 'basketPage',
         params: {
