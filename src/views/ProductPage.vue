02e945a8ffb38c1d7637f3df2045322bc7b090ad
Edgar Pan <edgaryp@me.com>
2018-11-22 22:27:17 +1100

    Added carousel with vueper-slides

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -2,16 +2,18 @@
   <v-content>
     <v-container>
       <v-layout row wrap>
-        <v-flex md6 sm12>
-            <v-carousel ref="carousel" :cycle="autoplay">
-              <v-carousel-item v-for="(image, index) in images" :key="index" :src="image"></v-carousel-item>
-            </v-carousel>
-            <!-- <h1>Product: {{currentProductName}}</h1>
-            <p>{{currentProduct}}</p>
-            <HelloWorld :currentProduct="currentProduct"/>
-            <button @click="test">Checkout</button> -->
+        <v-flex md6 sm12 xs12>
+          <div class="carousel">
+            <vueper-slides fixed-height="394px" :bullets="bullets" @slide="logEvents">
+                <vueper-slide v-for="(image, index) in images" :key="index" :image="image">
+                </vueper-slide>
+            </vueper-slides>
+            <div class="carousel-caption pt-2 pb-2">
+              <p class="mb-0">cunt</p>
+            </div>
+          </div>
         </v-flex>
-        <v-flex md6 sm12>
+        <v-flex md6 sm12 xs12>
         </v-flex>
       </v-layout>
     </v-container>
@@ -24,6 +26,9 @@ import {mapState, mapMutations, mapGetters} from 'vuex'
 import * as mutationTypes from '../store/mutation-types'
 import * as getterTypes from '../store/getter-types'
 // import HelloWorld from '@/components/ProductPage/HelloWorld.vue'
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.min.css'
+
 
 export default {
   data() {
@@ -36,13 +41,15 @@ export default {
         'https://picsum.photos/525/394?image=543',
         'https://picsum.photos/525/394?image=1047'
       ],
-      autoplay: false
+      bullets: false
     }
   },
   props: [
     'currentProductName' // from router
   ],
   components: {
+    VueperSlides,
+    VueperSlide
     // HelloWorld
   },
   computed: {
@@ -64,6 +71,10 @@ export default {
           shit: 'cunt'
         }
       });
+    },
+    logEvents (eventName, params) {
+      console.log({eventName, params});
+      this.events += ` ${eventName},  ${JSON.stringify(params, null, 0)}`
     }
   },
   created() {
@@ -71,3 +82,22 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+.carousel {
+  position: relative;
+  &-caption {
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+    text-align: center;
+    z-index: 10;
+    background-color: rgba(255, 255, 255, 0.5);
+    display: flex;
+    justify-content: center;
+    p {
+      color: white;
+    }
+  }
+}
+</style>
